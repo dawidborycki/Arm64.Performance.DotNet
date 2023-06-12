52c73d8da5ae158b0e1e694bab61c55b323e13c4
Dawid Borycki <dawid@borycki.com.pl>
2023-06-12 12:25:19 +0200

    Corrections

--- Program.cs
+++ Program.cs
@@ -3,16 +3,15 @@
 Console.WriteLine($"Processor architecture: " +
     $"{Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")}");
 
-const int executionCount = 500;
 const int trialCount = 5;
 
 for ( int i = 0; i < trialCount; i++ )
 {
     Console.WriteLine($"Trial no: {i + 1} / {trialCount}");
 
-    PerformanceHelper.MeasurePerformance(PerformanceTests.ListSorting, executionCount, "List sorting");
+    PerformanceHelper.MeasurePerformance(PerformanceTests.ListSorting, executionCount: 500, "List sorting");
 
-    PerformanceHelper.MeasurePerformance(PerformanceTests.ListSorting, executionCount, "Matrix multiplication");
+    PerformanceHelper.MeasurePerformance(PerformanceTests.SquareMatrixMultiplication, executionCount: 5, "Matrix multiplication");
 
-    PerformanceHelper.MeasurePerformance(PerformanceTests.StringOperations, executionCount*1000, "String operations");
+    PerformanceHelper.MeasurePerformance(PerformanceTests.StringOperations, executionCount: 500000, "String operations");
 }
